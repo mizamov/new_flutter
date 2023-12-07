39f14504150cc06b6470a6eed556256e6fd2fc19
mizamov <2110113@newuu.uz>
2023-12-07 14:48:31 +0500

    Initial commit

---
+++ lib/InputSection.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class InputSection extends StatelessWidget{
+  const InputSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.grey[200] ?? Colors.grey)!,
+              )
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: "Enter your email",
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.grey[200] ?? Colors.grey)!,
+              )
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: "Enter your password",
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
